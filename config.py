ed6a08d3ece2146b5f698032ea78bdf86eca425e
kripanidhid-beep <kripanidhid@gmail.com>
2026-01-15 21:57:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_STRING = os.getenv("SESSION_STRING")
+OWNER_ID = int(os.getenv("OWNER_ID"))
